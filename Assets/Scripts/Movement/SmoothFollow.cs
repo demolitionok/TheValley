6272a4b35aa99602de1759418d8b3487a509eb82
demolitionok <51877153+demolitionok@users.noreply.github.com>
2022-02-08 21:29:38 +0200

    feat: make camera smoothing follow character

---
+++ Assets/Scripts/Movement/SmoothFollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        var newX = Mathf.Lerp(transform.position.x, target.position.x, speed);
+        var newY = Mathf.Lerp(transform.position.y, target.position.y, speed);
+        transform.position = new Vector3(newX, newY, transform.position.z);
+    }
+}
